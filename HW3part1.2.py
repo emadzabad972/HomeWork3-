8be4efb71f3237426aa76e82b558ccb76f167096
emadzabad972 <emadzabad972@gmail.com>
2024-09-07 18:58:21 +0300

    Add files via upload

---
+++ HW3part1.2.py
@@ -0,0 +1,34 @@
+# start
+
+id_num1: str = input("whats your id number? ")
+
+name1: str = input("whats your name? ")
+
+height1: float = float(input("whats your height? "))
+
+weight1: int = int(input("whats your weight? "))
+
+id_num2: str = input("whats your id number? ")
+
+name2: str = input("whats your name? ")
+
+height2: float = float(input("whats your height? "))
+
+weight2: int = int(input("whats your weight? "))
+
+id_num3: str = input("whats your id number? ")
+
+name3: str = input("whats your name? ")
+
+height3: float = float(input("whats your height? "))
+
+weight3: int = int(input("whats your weight? "))
+
+print(f"person 1 : {id_num1:<10} {name1:<15} {height1:<10} {weight1:<10}")
+
+print(f"person 2 : {id_num2:<10} {name2:<15} {height2:<10} {weight2:<10}")
+
+print(f"person 3 : {id_num3:<10} {name3:<15} {height3:<10} {weight3:<10}")
+
+
+# end
---
+++ HW3part1.py
@@ -0,0 +1,17 @@
+# start
+
+id_num: str = input("whats your id number? ")
+
+f_name: str = input("whats your first name? ")
+
+l_name: str = input("whats your last name? ")
+
+height: float = float(input("whats your height? "))
+
+weight: int = int(input("whats your weight? "))
+
+print(f"id:{id_num},name:{l_name},{f_name} height:{height},weight:{weight}")
+
+# end
+
+
